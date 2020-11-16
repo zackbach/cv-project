a4f94bb15fea8857a2b2e7a2292a6f1676f84e74
Zack Eisbach <zackbach@gmail.com>
2020-11-16 17:39:56 -0500

    updated date format

--- src/components/JobDisplay.js
+++ src/components/JobDisplay.js
@@ -7,12 +7,15 @@ class JobDisplay extends Component {
 
   render() {
     const { job, handleFormOpen } = this.props;
+    const startDate = new Date(job.start)
+    const endDate = new Date(job.end)
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
 
     return(
       <div className="mx-3">
         <p className="mb-0">{job.name}</p>
         <p className="mb-0">{job.position}</p>
-        <p className="mb-2">Worked from {job.start} to {job.end}</p>
+        <p className="mb-2">Worked from {new Intl.DateTimeFormat('en-US', options).format(startDate)} to {new Intl.DateTimeFormat('en-US', options).format(endDate)}</p>
         <button onClick={handleFormOpen} className="btn btn-outline-primary mb-1">Edit</button>
       </div>
     )
--- src/components/SchoolDisplay.js
+++ src/components/SchoolDisplay.js
@@ -7,12 +7,15 @@ class SchoolDisplay extends Component {
 
   render() {
     const { school, handleFormOpen } = this.props;
+    const startDate = new Date(school.start)
+    const endDate = new Date(school.end)
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
 
     return(
       <div className="mx-3">
         <p className="mb-0">{school.name}</p>
         <p className="mb-0">{school.degree}</p>
-        <p className="mb-2">Attended from {school.start} to {school.end}</p>
+        <p className="mb-2">Attended from {new Intl.DateTimeFormat('en-US', options).format(startDate)} to {new Intl.DateTimeFormat('en-US', options).format(endDate)}</p>
         <button onClick={handleFormOpen} className="btn btn-outline-primary mb-1">Edit</button>
       </div>
     )
